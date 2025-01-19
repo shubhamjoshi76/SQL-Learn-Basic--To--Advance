3d02db7d1288e04d42c6bad17d47dc258c61abd3
shubhamjoshi76 <shubhamjoshi2019@gmail.com>
2025-01-19 10:44:47 +0530

    Table created

---
+++ SQL Commond.sql
@@ -0,0 +1,12 @@
+create database Batch24
+use Batch24
+
+Create Table student(
+RollNumber int,
+Name Varchar(20),
+city Varchar (20),
+Email Varchar(20))
+
+Select * From student
+
+insert into student values (1 ,'Shubham', 'Pune' , 'shubham@gmail.com')
